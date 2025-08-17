0292183e1fdbd1fe7e083dee4f37da454725fb5e
Lenin Mombe <leninmombe91@gmail.com>
2025-08-17 11:15:07 +0200

    Create task_3.sql

---
+++ task_3.sql
@@ -0,0 +1,7 @@
+-- This script lists all tables in the specified database
+
+-- Use the database specified by the user
+USE alx_book_store;
+
+-- List all the tables in the selected database
+SHOW TABLES;
